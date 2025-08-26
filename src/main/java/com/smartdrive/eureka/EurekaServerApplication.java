8d8661fcf56ed9523a3355da883efb9abef556ec
Dev-Mrignaka <mrigankamondal10@gmail.com>
2025-08-26 23:33:50 +0530

    Initial commit: SmartDrive Eureka Server
    - Spring Cloud Netflix Eureka Server
    - Service discovery and registration
    - Docker support with Dockerfile
    - Gradle build configuration
    - Application configuration for service registry

---
+++ src/main/java/com/smartdrive/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.smartdrive.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server Application
+ * Service discovery server for the SmartDrive microservices
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
